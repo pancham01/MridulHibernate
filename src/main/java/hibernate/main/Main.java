ff7fa0972702e7100c2fffe869422d3744173250
pancham01 <pancham9557@gmail.com>
2024-11-23 15:35:37 +0530

    update

--- src/main/java/hibernate/main/Main.java
+++ src/main/java/hibernate/main/Main.java
@@ -2,19 +2,15 @@ package hibernate.main;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 import hibernate.entity.Employee;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Employee em = new Employee( "Riya", "Female", 57890);
+		Employee em = new Employee(1, "Riya", "Female", 57890);
 
 //		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfgg.xml").build();
 //		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
