2889643bfb0a242ca610e4d959df290d1844b855
pancham01 <pancham9557@gmail.com>
2024-11-23 16:05:41 +0530

    update

---
+++ src/main/java/hibernate/config/HibernateConfiguration.java
@@ -0,0 +1,29 @@
+package hibernate.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+public class HibernateConfiguration {
+
+	public static SessionFactory getSessionFactory() {
+		Properties ps = new Properties();
+		ps.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		ps.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/riya");
+		ps.put(Environment.USER, "root");
+		ps.put(Environment.PASS, "root");
+		ps.put(Environment.HBM2DDL_AUTO, "create");
+		ps.put(Environment.SHOW_SQL, "true");
+		ps.put(Environment.FORMAT_SQL, "true");
+
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(ps).build();
+		MetadataSources meta = new MetadataSources(ssr);
+		meta.addAnnotatedClassName("hibernate.entity.Employee");
+//		SessionFactory sf = meta.buildMetadata().buildSessionFactory();
+		return meta.buildMetadata().buildSessionFactory();
+	}
+}
--- src/main/java/hibernate/main/Main.java
+++ src/main/java/hibernate/main/Main.java
@@ -2,23 +2,18 @@ package hibernate.main;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import hibernate.config.HibernateConfiguration;
 import hibernate.entity.Employee;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Employee em = new Employee(1, "Riya", "Female", 57890);
-
-//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfgg.xml").build();
-//		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-//		SessionFactory sf = meta.buildSessionFactory();
-		SessionFactory sf = new MetadataSources( new StandardServiceRegistryBuilder().configure("hibernate.cfgg.xml").build()).getMetadataBuilder().build().buildSessionFactory();
+		Employee em = new Employee("Riya", "Female", 57890);
+		SessionFactory sf = HibernateConfiguration.getSessionFactory();
 		
 		Session session = sf.openSession();
-		session.save(em);
+		session.persist(em);
 		session.beginTransaction().commit();
 		sf.close();
 		System.out.println("Main.main()");
